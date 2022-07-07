6999e054012941b226093f21d1d400f5912a4c75
diego.castano <diegocast5566@gmail.com>
2022-07-06 21:30:25 -0500

    mi primer commit

---
+++ js.js
@@ -0,0 +1,33 @@
+/*
+7.	Utilizando el código anexo, poner el evento click al boton del menu movil para que puedan mostrarse los enlaces del menú.  código ejercicio
+Resultado.
+
+*/
+
+const $elementoicon=document.querySelector(".icon");
+
+
+
+
+$elementoicon.addEventListener("click",function(){
+
+    const $otro = document.querySelector("#myNavbar");
+    
+
+    if($otro){
+
+        $otro.classList.toggle("responsive");
+
+    }
+
+    
+});
+
+
+
+
+
+
+
+
+
